9cb5cba6a1fc0aa0ee6ede5f714cefe038c22db8
Srikanth <Gali_Srikanth@msrlimited.com>
2020-01-28 15:13:15 +0530

    Initial files of project setup

---
+++ sreeProjectSetup/MVC/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  sreeProjectSetup
+//
+//  Created by Gali Srikanth on 28/01/20.
+//  Copyright © 2020 Msrit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Project setup ready")
+    }
+}
